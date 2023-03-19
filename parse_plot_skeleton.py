e28a1e7e8301fa59b37edce5f142119dfc8e90dd
codydev901 <codydev901@gmail.com>
2023-03-19 16:19:43 -0400

    final commit

--- parse_plot_skeleton.py
+++ parse_plot_skeleton.py
@@ -1,7 +1,15 @@
 import numpy as np
+import pandas as pd
 import open3d as o3d
 
 """
+Cody Whitt
+pkz325
+CPSC 4530 Spring 2023
+Assignment 3
+
+For DataSet 3 - Scientific
+
 Colors
 https://github.com/isl-org/Open3D/issues/614
 """
@@ -44,6 +52,50 @@ def exploratory_get_wound_region_coordinates():
                                       # up=[-0.0694, -0.9768, 0.2024])
 
 
+def rgb_from_df_percent(df: pd.DataFrame, wound_location: str):
+
+    wound_percent = df[df["wound_location"] == wound_location]
+    wound_percent = list(wound_percent["relative_survival_rate_adjusted"])[0]
+
+    red_percent = 1.0 - wound_percent
+
+    return [red_percent, wound_percent, 0.0]
+
+
+def plot_skeleton(civil_war: str):
+
+    print(f"Generating Figure For:{civil_war}")
+
+    df = pd.read_csv("parsed_data/civil_war_parsed.csv")
+    df = df[df["civil_war"] == civil_war]
+    pcd = o3d.io.read_point_cloud("raw_data/Human skeleton.ply")
+
+    head_color = rgb_from_df_percent(df, "head")
+    extremities_color = rgb_from_df_percent(df, "extremity")
+    abdomen_color = rgb_from_df_percent(df, "abdomen")
+    chest_color = rgb_from_df_percent(df, "chest")
+
+    new_colors = []
+    for i, p in enumerate(pcd.points):
+        if p[2] > 600:  # Head And Neck (Head)
+            new_colors.append(head_color)
+        elif p[2] < 5:  # Legs/Hands (Extremities)
+            new_colors.append(extremities_color)
+        elif p[0] < -93 or p[0] > 143:  # Arms (Extremities)
+            new_colors.append(extremities_color)
+        elif p[2] < 300:  # Abdomen
+            new_colors.append(abdomen_color)
+        else:  # Chest
+            new_colors.append(chest_color)
+
+    pcd.colors = o3d.utility.Vector3dVector(np.asarray(new_colors))
+    o3d.visualization.draw_geometries([pcd])
+
+
 if __name__ == "__main__":
 
-    exploratory_get_wound_region_coordinates()
+    # exploratory_get_wound_region_coordinates()
+
+    plot_skeleton(civil_war="United States")
+
+    # plot_skeleton(civil_war="Syria")
